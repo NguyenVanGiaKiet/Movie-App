c9d871a9bd2ddf6a6be153a6e8adcba7098d3f10
Gia Kiet <fancr7qwer@gmail.com>
2026-03-18 02:42:16 +0700

    add loading

--- backend/seed-movies/seed-movies.js
+++ backend/seed-movies/seed-movies.js
@@ -36,31 +36,33 @@ const Movie = mongoose.model("Movie", movieSchema);
 
 // 📥 DATA (bạn có thể thay bằng file JSON hoặc API)
 const movieData = {
-    name: "Thế Giới Không Lối Thoát (Phần 3)",
-    origin_name: "Alice In Borderland (Season 3)",
-    slug: "the-gioi-khong-loi-thoat-phan-3",
-    content: "Ở Mùa 3, họ đã kết hôn và sống một cuộc sống bình thường, nhưng ký ức về Borderland thi thoảng hiện lên qua giấc mơ hoặc ảo giác.",
-    type: "phim-bo",
+    name: "Arcane: Liên Minh Huyền Thoại (Phần 2)",
+    origin_name: "Arcane: League of Legends (season 2)",
+    slug: "arcane-lien-minh-huyen-thoai-phan-2",
+    content: "Các liên minh hình thành, lòng trung thành vụn vỡ và mối nguy hiểm mới xuất hiện khi trận chiến giữa Piltover và Zaun mang đến cả vinh quang lẫn đau thương.",
+    type: "tv-show",
     status: "completed",
-    thumb_url: "https://image.tmdb.org/t/p/original/1B1hllL2Fjap1f2EUsHjBqny3Kg.jpg",
-    poster_url: "https://image.tmdb.org/t/p/original/c8WeahQQ9DbKOln4JKTZ5flg1Qe.jpg",
-    trailer_url: "https://www.youtube.com/embed/-gPsHwkiXwI",
-    time: "50 phút/tập",
-    episode_current: "Hoàn tất (8/8)",
-    episode_total: "8",
+    thumb_url: "https://image.tmdb.org/t/p/original/oTGEwkp1mPgWNMgVSM53TWfzgSc.jpg",
+    poster_url: "https://image.tmdb.org/t/p/original/27yH7q4nYadsbF7wMUt5xQG2tYN.jpg",
+    trailer_url: "https://www.youtube.com/embed/ysqiEC6bLUI",
+    time: "40 phút/tập",
+    episode_current: "Hoàn tất (9/9)",
+    episode_total: "9",
     quality: "HD",
     lang: "Vietsub",
-    year: 2025,
+    year: 2024,
     category: [
         "69b26ebd233f020096e6d70c",
-        "69b26ebd233f020096e6d718",
-        "69b26ebd233f020096e6d713"
+        "69b26ebd233f020096e6d70d",
+        "69b26ebd233f020096e6d70e",
+        "69b26ebd233f020096e6d722"
     ],
     country: [
-        "69b26ebd233f020096e6d71b"
+        "69b26ebd233f020096e6d718",
+        "69b26ebd233f020096e6d71e"
     ],
-    director: ["Shinsuke Sato"],
-    actor: ["Yamazaki Kento", "Tsuchiya Tao", "Murakami Nijiro"],
+    director: ["Pascal Charrue","Arnaud Delord"],
+    actor: ["Hailee Steinfeld", "Kevin Alejandro", "Jason Spisak"],
     episodes: [
         {
             server_name: "Server Vietsub",
@@ -68,32 +70,47 @@ const movieData = {
   {
     "name": "Tập 1",
     "slug": "tap-1",
-    "link_embed": "https://embed12.streamc.xyz/embed.php?hash=c34dcd68cf9c87441dc106bf5de115d5"
+    "link_embed": "https://embed12.streamc.xyz/embed.php?hash=12850906d46de144a8f37e0961528388"
   },
   {
     "name": "Tập 2",
     "slug": "tap-2",
-    "link_embed": "https://embed13.streamc.xyz/embed.php?hash=1300dc9f0c2fe0bc8963044549011235"
+    "link_embed": "https://embed11.streamc.xyz/embed.php?hash=ef92ba3c319352b44c7d8467067d40be"
   },
   {
     "name": "Tập 3",
     "slug": "tap-3",
-    "link_embed": "https://embed11.streamc.xyz/embed.php?hash=653841dfcef2ccd85f4dee1e7961019d"
+    "link_embed": "https://embed11.streamc.xyz/embed.php?hash=1594db360361adedbc7e53dc31a1a11e"
   },
   {
     "name": "Tập 4",
     "slug": "tap-4",
-    "link_embed": "https://embed11.streamc.xyz/embed.php?hash=86c94b7fd8750d45cedc58b935620a76"
+    "link_embed": "https://embed15.streamc.xyz/embed.php?hash=837a633b4e13c9a8630df609f25f7a0c"
   },
   {
     "name": "Tập 5",
     "slug": "tap-5",
-    "link_embed": "https://embed12.streamc.xyz/embed.php?hash=da800442baea5c2b8194bb0b5319fad5"
+    "link_embed": "https://embed17.streamc.xyz/embed.php?hash=e4a5cd8b52f8564da8727a4d54cd376c"
   },
   {
     "name": "Tập 6",
     "slug": "tap-6",
-    "link_embed": "https://embed11.streamc.xyz/embed.php?hash=b902fd3cff24c686ae960a12eee71d89"
+    "link_embed": "https://embed14.streamc.xyz/embed.php?hash=79536b30cc9858cb4466562a363cea51"
+  },
+  {
+    "name": "Tập 7",
+    "slug": "tap-7",
+    "link_embed": "https://embed14.streamc.xyz/embed.php?hash=6e36df188ae4d39b89c3c61cec199f3c"
+  },
+  {
+    "name": "Tập 8",
+    "slug": "tap-8",
+    "link_embed": "https://embed17.streamc.xyz/embed.php?hash=e9bbe768667e54ae6b8f4c0a1bc04f0b"
+  },
+  {
+    "name": "Tập 9",
+    "slug": "tap-9",
+    "link_embed": "https://embed14.streamc.xyz/embed.php?hash=e541651f45b7206c7b35c89852a277be"
   }
 ],
         },
